9d24acffbba43646e18efe65dfc109708a8bc377
mooreBrendan <moore.brendanm@comcast.net>
2025-05-02 21:51:14 -0400

    add comments

--- files/update.sh
+++ files/update.sh
@@ -1,10 +1,14 @@
+#run updates
 apt-get update
 apt-get upgrade -y
 
+#check if docker is running and prune docker if it is
 out=""
 if [ "$(systemctl is-active docker)" = "active" ]; then
   docker system prune -f
 	out=" and pruned"
 fi
+
+#notify ntfy that this host ran this script
 hn=$(hostname -f)
 curl -d "updated$out $hn" -k -L ntfy.bmoore.tech/server-notifs
